5dc2bba37118ed3202017b030cf60f06dd93656f
nemo <justformygithub@gmail.com>
2024-11-07 12:03:09 +0330

    update characters

--- components/AvatarList.jsx
+++ components/AvatarList.jsx
@@ -5,14 +5,12 @@ export default function AvatarList({ selected, isBackwards, style }) {
     if (position < 0) position += 11; /* length of array */
 
     const list = Array.from(Array(5), (_, i) => {
+        
         const item = <div
             className={i === offset ? (isBackwards ? style.selectedAvatarContainerB : style.selectedAvatarContainerF) : style.avatarContainer} key={position} >
-            <img
-            src={`/avatars/${position}.webp`}
-            className={`${i === offset ? style.selectedAvatar : style.avatar} ${i < offset ? style.prevAvatar : style.nextAvatar}`}
-            draggable="false"
-            loading="lazy"
-        />
+            <div
+            className={`${i === offset ? style.selectedAvatar : style.avatar} ${i < offset ? style.prevAvatar : style.nextAvatar} ${style[`avatar${position}`]}`}
+            />
         </div>;
 
         position = (position + 1) % 11;
