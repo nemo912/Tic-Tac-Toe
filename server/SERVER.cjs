a09c6f78fd7cdb23631b107a0c294f7e50fdb50a
nemo <justformygithub@gmail.com>
2024-11-07 11:12:30 +0330

    add express

--- server/SERVER.cjs
+++ server/SERVER.cjs
@@ -1,5 +1,8 @@
 const http = require('node:http');
 const fs = require('node:fs');
+const express = require('express');
+
+const app = express();
 
 const avatars = {};
 const _avas = Array.from(Array(11), (_, i) => {
@@ -17,25 +20,43 @@ const routes = {
     '/index.js': fs.readFileSync('./lib/index.js'),
 }
 
-
-const server = http.createServer((req, res) => {
-    const { method, url } = req;
-
-    if (method === 'GET') {
-        switch (url) {
-            case '/': res.end(routes[url]);
-                break;
-            case '/main.css': res.end(routes[url]);
-                break;
-            case '/index.js': res.end(routes[url])
-                break;
-            default:
-                if (url.startsWith('/avatars/') && url.endsWith('.webp')) {
-                    res.end(routes.avatars[url]);
-                }
-        }
-    }
+app.get('/', (req, res) => {
+    res.end(routes['/']);
 });
 
-server.listen(80, '0.0.0.0', () => { console.log('server is running...', server.address()) });
+app.get('/main.css', (req, res) => {
+    res.end(routes['/main.css']);
+});
+
+app.get('/index.js', (req, res) => {
+    res.end(routes['/index.js']);
+});
+
+app.get(/\/avatars\/[0-9]|10.webp/, (req, res) => {
+    res.end(routes.avatars[req.path]);
+});
+
+app.listen(80, () => console.log('server is running...'));
+
+
+// const server = http.createServer((req, res) => {
+//     const { method, url } = req;
+
+//     if (method === 'GET') {
+//         switch (url) {
+//             case '/': res.end(routes[url]);
+//                 break;
+//             case '/main.css': res.end(routes[url]);
+//                 break;
+//             case '/index.js': res.end(routes[url])
+//                 break;
+//             default:
+//                 if (url.startsWith('/avatars/') && url.endsWith('.webp')) {
+//                     res.end(routes.avatars[url]);
+//                 }
+//         }
+//     }
+// });
+
+// server.listen(80, '0.0.0.0', () => { console.log('server is running...', server.address()) });
 
