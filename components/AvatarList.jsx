8e9d63ef65fcb04170d57b21897a3131266255e3
nemo <justformygithub@gmail.com>
2024-11-07 15:57:39 +0330

    final commit!!! version 2.0.0

--- components/AvatarList.jsx
+++ components/AvatarList.jsx
@@ -1,10 +1,10 @@
 export default function AvatarList({ selected, isBackwards, style }) {
-    const offset = 2;
+    const offset = 3;
 
     let position = selected - offset;
     if (position < 0) position += 11; /* length of array */
 
-    const list = Array.from(Array(5), (_, i) => {
+    const list = Array.from(Array(7), (_, i) => {
         const item = <div
             className={i === offset ? (isBackwards ? style.selectedAvatarContainerB : style.selectedAvatarContainerF) : style.avatarContainer} key={position} >
             <img
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,4 @@ import { createRoot } from 'react-dom/client';
 import App from '@compo/App.jsx';
 import '@styl/global.css';
 
-for (let i = 3; i < 9; i++) fetch(`/avatars/${i}.webp`);
-
 createRoot(document.querySelector('.root-container')).render(<App />);
