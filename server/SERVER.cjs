68328470a5d1df02d0c7faf4ce83b4aeebdd2c0c
nemo <justformygithub@gmail.com>
2024-11-07 10:24:19 +0330

    server buffers

--- server/SERVER.cjs
+++ server/SERVER.cjs
@@ -1,20 +1,37 @@
 const http = require('node:http');
 const fs = require('node:fs');
 
+const avatars = {};
+const _avas = Array.from(Array(11), (_, i) => {
+    return fs.readFileSync(`./lib/avatars/${i}.webp`);
+});
+
+_avas.forEach((avaii, i) => {
+    avatars[`/avatars/${i}.webp`] = avaii;
+});
+
+const routes = {
+    avatars: avatars,
+    '/': fs.readFileSync('./lib/index.html'),
+    '/main.css': fs.readFileSync('./lib/main.css'),
+    '/index.js': fs.readFileSync('./lib/index.js'),
+}
+
+
 const server = http.createServer((req, res) => {
     const { method, url } = req;
 
     if (method === 'GET') {
         switch (url) {
-            case '/': fs.createReadStream('./lib/index.html').pipe(res);
+            case '/': res.end(routes[url]);
                 break;
-            case '/main.css': fs.createReadStream('./lib/main.css').pipe(res);
+            case '/main.css': res.end(routes[url]);
                 break;
-            case '/index.js': fs.createReadStream('./lib/index.js').pipe(res);
+            case '/index.js': res.end(routes[url])
                 break;
             default:
                 if (url.startsWith('/avatars/') && url.endsWith('.webp')) {
-                    fs.createReadStream(`./lib${url}`).pipe(res);
+                    res.end(routes.avatars[url]);
                 }
         }
     }
